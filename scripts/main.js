58902883f32120acaa97f5c646f30aec481f1282
mooneykohner <mooneykohner@gmail.com>
2024-05-17 14:35:27 -0500

    Javascript stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world! This is a test, and running this should change the heading labeled 'subjects'."
+myHeading.textContent = "Hello world!"
+
+document.getElementById("test").innerHTML = "This is not a test."
+
+function onButtonClick() {
+    alert("Surprise!")
+}
+
+const button = document.getElementById("button1")
+button.addEventListener("click", onButtonClick)
