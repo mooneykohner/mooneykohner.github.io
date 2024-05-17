b21fc1565fc250d1070e2abbef0373afbb2bf5fb
mooneykohner <mooneykohner@gmail.com>
2024-05-17 15:46:44 -0500

    Javascript stuff

--- scripts/button.js
+++ scripts/button.js
@@ -1,11 +1,13 @@
 function buttonClicked () {
     const paraChange = document.getElementById("test")
+    const playAudio = document.getElementById("dingButton")
     if (paraChange.textContent === "This is a test") {
         paraChange.textContent = "Hello"
     }
     else {
         paraChange.textContent = "This is a test"
     }
+    playAudio.play();
 }
 
 const buttonPress = document.getElementById("pressMe")
