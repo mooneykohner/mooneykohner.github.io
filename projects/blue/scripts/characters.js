54f3c6ea12b206c5f52a1a6304c9509a2aa78344
mooneykohner <mooneykohner@gmail.com>
2024-05-24 01:58:35 -0500

    Projects

---
+++ projects/blue/scripts/characters.js
@@ -0,0 +1,10 @@
+function buttonChar1 () {
+    const imageBlue = document.getElementById ("characterBox")
+    const characterPara = document.getElementById ("characterPara")
+
+    characterPara.textContent = "She's so cool!"
+    imageBlue.src = "QuestionMark2.png"
+}
+
+const buttonBlue = document.getElementById ("characterBlue")
+buttonBlue.addEventListener("click", buttonChar1)
