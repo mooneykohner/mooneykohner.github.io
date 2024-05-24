253bccaa93c0cdf0026f4a97a331d395d9a2c188
mooneykohner <mooneykohner@gmail.com>
2024-05-24 15:38:27 -0500

    Projects

--- projects/blue/scripts/characters.js
+++ projects/blue/scripts/characters.js
@@ -2,9 +2,14 @@ function buttonChar1 () {
     const imageBlue = document.getElementById ("characterImage")
     const characterPara = document.getElementById ("characterPara")
 
+    imageBlue.style.transition = "transform 0.6"
     imageBlue.style.transform = "rotateY(180deg)";
-    imageBlue.src = "../blue/images/QuestionMark2.png";
-    characterPara.textContent = "She's so cool!";
+
+    setTimeout(() => {
+        imageBlue.src = "../blue/images/QuestionMark2.png";
+        characterPara.textContent = "She's so cool!";
+        imageBlue.style.transform = "rotateY(0deg)";
+    }, 600);
 }
 
 const buttonBlue = document.getElementById ("characterBlue")
