e4e902b4dddbd7eddef3e8a334734c275dfd0ac2
mooneykohner <mooneykohner@gmail.com>
2024-05-24 15:49:08 -0500

    Projects

--- projects/blue/scripts/characters.js
+++ projects/blue/scripts/characters.js
@@ -1,10 +1,21 @@
 function buttonChar1 () {
-    const imageBlue = document.getElementById ("characterImage")
+    const characterImage = document.getElementById ("characterImage")
     const characterPara = document.getElementById ("characterPara")
 
+    characterImage.src = "../blue/images/QuestionMark2.png";
     characterPara.textContent = "She's so cool!";
-    imageBlue.src = "../blue/images/QuestionMark2.png";
+}
+
+function buttonChar2() {
+    const characterImage = document.getElementById ("characterImage")
+    const characterPara = document.getElementById ("characterPara")
+
+    characterImage.src = "..blue/images/QuestionMark.png";
+    characterPara.textContent = "She's a super detective!";
 }
 
 const buttonBlue = document.getElementById ("characterBlue")
 buttonBlue.addEventListener("click", buttonChar1)
+
+const buttonJuniper = document.getElementById ("characterJuniper")
+buttonJuniper.addEventListener("click", buttonChar2)
