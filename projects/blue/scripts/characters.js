a3740b8d8e06b5fb1c66706d8cbaa577d854a48e
mooneykohner <mooneykohner@gmail.com>
2024-05-25 02:43:54 -0500

    Projects

---
+++ projects/blue/scripts/characters.js
@@ -0,0 +1,27 @@
+var characterImage = document.getElementById("characterImage");
+var characterPara = document.getElementById("characterPara");
+function buttonChar1() {
+    characterImage.src = "../blue/images/QuestionMark.png";
+    characterPara.textContent = "She's so cool!";
+    characterImage.title = "The sweetest superhero!";
+}
+function buttonChar2() {
+    characterImage.src = "../blue/images/QuestionMark.png";
+    characterPara.textContent = "She's a super detective!";
+}
+function buttonChar3() {
+    characterImage.src = "../blue/images/QuestionMark.png";
+    characterPara.textContent = "He's an elite hacker!";
+}
+function buttonChar4() {
+    characterImage.src = "../blue/images/QuestionMark.png";
+    characterPara.textContent = "She's a mad chemist!";
+}
+var buttonBlue = document.getElementById("characterBlue");
+buttonBlue.addEventListener("click", buttonChar1);
+var buttonJuniper = document.getElementById("characterJuniper");
+buttonJuniper.addEventListener("click", buttonChar2);
+var buttonVincent = document.getElementById("characterVincent");
+buttonVincent.addEventListener("click", buttonChar3);
+var buttonOlive = document.getElementById("characterOlive");
+buttonOlive.addEventListener("click", buttonChar4);
