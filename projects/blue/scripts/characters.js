8282822b5553603e1f627395bec829d53f69ec58
mooneykohner <mooneykohner@gmail.com>
2024-05-30 00:39:44 -0500

    Projects

--- projects/blue/scripts/characters.js
+++ projects/blue/scripts/characters.js
@@ -1,30 +1,20 @@
-function buttonChar1() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's so cool!";
-    characterPara.textContent = "She's so cool!";
-}
-function buttonChar2() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's a super detective!";
-    characterPara.textContent = "She's a super detective!";
-}
-function buttonChar3() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "He's an elite hacker!";
-    characterPara.textContent = "He's an elite hacker!";
-}
-function buttonChar4() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's a mad chemist!";
-    characterPara.textContent = "She's a mad chemist!";
-}
 var characterPara = document.getElementById("characterPara");
 var characterImage = document.getElementById("characterImage");
-var buttonBlue = document.getElementById("characterBlue");
-buttonBlue.addEventListener("click", buttonChar1);
-var buttonJuniper = document.getElementById("characterJuniper");
-buttonJuniper.addEventListener("click", buttonChar2);
-var buttonVincent = document.getElementById("characterVincent");
-buttonVincent.addEventListener("click", buttonChar3);
-var buttonOlive = document.getElementById("characterOlive");
-buttonOlive.addEventListener("click", buttonChar4);
+function characterInfo(imageSrc, title, textContent) {
+    characterImage.src = imageSrc;
+    characterImage.title = title;
+    characterPara.textContent = textContent;
+}
+var buttonContainer = document.getElementById("buttonContainer");
+if (buttonContainer) {
+    var characterButtons = buttonContainer.querySelectorAll("button");
+    characterButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            switch (button.id) {
+                case "characterBlue":
+                    characterInfo("../images/QuestionMark.png", "She's so cool!", "She's so cool!");
+                    break;
+            }
+        });
+    });
+}
--- projects/blue/scripts/characters.ts
+++ projects/blue/scripts/characters.ts
@@ -1,39 +1,26 @@
-function buttonChar1 () {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's so cool!";
-    characterPara.textContent = "She's so cool!";
-}
-
-function buttonChar2() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's a super detective!";
-    characterPara.textContent = "She's a super detective!";
-}
-
-function buttonChar3() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "He's an elite hacker!";
-    characterPara.textContent = "He's an elite hacker!";
-}
-
-function buttonChar4() {
-    characterImage.src = "../images/QuestionMark.png";
-    characterImage.title = "She's a mad chemist!"
-    characterPara.textContent = "She's a mad chemist!"
-}
-
 const characterPara = document.getElementById ("characterPara") as HTMLParagraphElement;
-const characterImage = document.getElementById("characterImage") as HTMLImageElement;
+const characterImage =  document.getElementById ("characterImage") as HTMLImageElement;
 
+function characterInfo(imageSrc: string, title: string, textContent: string) {
+    characterImage.src = imageSrc;
+    characterImage.title = title;
+    characterPara.textContent = textContent;
+}
 
-const buttonBlue = document.getElementById ("characterBlue")
-buttonBlue.addEventListener("click", buttonChar1)
+const buttonContainer = document.getElementById("buttonContainer");
+if (buttonContainer) {
+    const characterButtons = buttonContainer.querySelectorAll("button");
 
-const buttonJuniper = document.getElementById ("characterJuniper")
-buttonJuniper.addEventListener("click", buttonChar2)
-
-const buttonVincent = document.getElementById ("characterVincent")
-buttonVincent.addEventListener("click", buttonChar3)
-
-const buttonOlive = document.getElementById ("characterOlive")
-buttonOlive.addEventListener ("click", buttonChar4)
+    characterButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            switch (button.id) {
+                case "characterBlue":
+                    characterInfo(
+                        "../images/QuestionMark.png",
+                        "She's so cool!",
+                        "She's so cool!")
+                        break;
+            }
+        })
+    })
+}
